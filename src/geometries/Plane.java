928e64d7763b63ab278f57ff8c09dbd493c52f4e
shire <shirelf15gmail.com>
2025-04-03 20:34:24 +0300

    comments

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -1,7 +1,10 @@
 package geometries;
 
 import primitives.Point;
+import primitives.Ray;
 import primitives.Vector;
+
+import java.util.List;
 
 /**
  * Represents a plane in 3D space defined by a point and a normal vector.
@@ -57,4 +60,8 @@ public class Plane extends Geometry {
         return normal;
     }
 
+    @Override
+    public List<Point> findIntsersections(Ray ray) {
+        return null;
+    }
 }
